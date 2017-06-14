b23dc2627b8bda760c46b7f50dff4a1c7c78e145
高凯 <kxjhaha@gmail.com>
2017-06-14 23:36:26 +0800

    自己实现的投影器

---
+++ Assets/Script/ShadowCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowCamera : MonoBehaviour {
+
+    Camera cam;
+    RenderTexture rt;
+	// Use this for initialization
+	void Start () {
+        GameObject go = new GameObject("Cam");
+        cam = go.AddComponent<Camera>();
+
+        cam.clearFlags = CameraClearFlags.Depth;
+
+        cam.orthographic = true;
+        cam.orthographicSize = 10;
+        cam.aspect = 1;
+        cam.backgroundColor = new Color(1, 1, 1, 0);
+        cam.transform.position = transform.position;
+        cam.transform.rotation = transform.rotation;
+        cam.transform.SetParent(transform);
+
+        rt = new RenderTexture(1024, 1024, 0);
+        rt.wrapMode = TextureWrapMode.Clamp;
+
+        cam.targetTexture = rt;
+        cam.cullingMask = LayerMask.GetMask("ShadowCaster");
+        cam.SetReplacementShader(Shader.Find("MyShaderLib/Render2Texture"), "RenderType");
+
+        cam.Render();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Shader/Render2Texture.shader
@@ -0,0 +1,58 @@
+﻿Shader "MyShaderLib/Render2Texture"
+{
+	Properties
+	{
+
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float2 depth:TEXCOORD1;
+				float4 vertex : SV_POSITION;
+			};
+
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				// o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				o.depth = o.vertex.zw;
+
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				float d = i.depth.x / i.depth.y; //介于0到1之间
+
+				// sample the texture
+				fixed4 col = EncodeFloatRGBA(d);
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
